5a37fe50aa3002b8e3ab3c0097519d3173c51214
neethuchandhavarkar <woofwoofpoodle@gmail.com>
2025-11-04 10:35:00 +1100

    Remove sensitive Firebase config from config.ts
    Removed Firebase configuration details for security.

--- firebase/config.ts
+++ firebase/config.ts
@@ -4,14 +4,5 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDtk8MlD_b-xie0HNpu0Xoaet3UyBefBhs",
-  authDomain: "prototype-becee.firebaseapp.com",
-  projectId: "prototype-becee",
-  storageBucket: "prototype-becee.firebasestorage.app",
-  messagingSenderId: "259788007017",
-  appId: "1:259788007017:web:707aab14e9d304791ea4f8"
-};
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
